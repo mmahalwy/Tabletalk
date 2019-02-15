775b42d400e27a23047abd3dceec4c541761ac02
Mohamed El Mahallawy <mmahalwy@gmail.com>
2019-02-14 21:04:13 -0800

    Add meta tags

--- Gemfile
+++ Gemfile
@@ -48,6 +48,7 @@ gem 'activerecord-session_store'
 gem 'sentry-raven'
 gem 'delayed_job_active_record'
 gem 'httparty'
+gem 'meta-tags'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,21 +1,43 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: site_brand_name,
+      description: site_description,
+      keywords: "coffee, business, professionals, linkedin",
+      image_src: site_home_image,
+      og: {
+        title: :title,
+        description: :description,
+        image: :image_src,
+        site_name: :site,
+      },
+      twitter: {
+        title: :title,
+        description: :description,
+        image: :image_src,
+        site_name: :site,
+      },
+      viewport: "width=device-width, initial-scale=1.0",
+    }
+  end
+
   def site_brand_name
-    'Tabletalk'
+    "Tabletalk"
   end
 
   def site_home_image
-    'https://images.unsplash.com/photo-1529022805552-1c88a713c1c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1225&q=80'
+    "https://images.unsplash.com/photo-1529022805552-1c88a713c1c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1225&q=80"
   end
 
   def site_description
-    'Meet, learn and grow. Every week, you will be matched with business professionals in your community for a cup of coffee.'
+    "Meet, learn and grow. Every week, you will be matched with business professionals in your community for a cup of coffee."
   end
 
   def active_class(link_path)
-    current_page?(link_path) ? 'active' : ''
+    current_page?(link_path) ? "active" : ""
   end
 
   def home_path?
-    current_page?('/')
+    current_page?("/")
   end
 end
